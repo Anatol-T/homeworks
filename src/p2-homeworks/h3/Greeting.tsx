59eba371b329a1985f867a6240e54eb8104e7e7e
Anatol-T <84692030+Anatol-T@users.noreply.github.com>
2021-11-25 21:15:18 +0800

    lesson3

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -1,28 +1,33 @@
-import React from 'react'
+import React, {ChangeEvent} from 'react'
 import s from './Greeting.module.css'
+import {errorType} from "./GreetingContainer";
 
 type GreetingPropsType = {
-    name: any // need to fix any
-    setNameCallback: any // need to fix any
-    addUser: any // need to fix any
-    error: any // need to fix any
-    totalUsers: any // need to fix any
+  name: string // need to fix any
+  setNameCallback: (e: ChangeEvent<HTMLInputElement>) => void// need to fix any
+  addUser: () => void // need to fix any
+  error: errorType // need to fix any
+  totalUsers: number // need to fix any
 }
 
 // презентационная компонента (для верстальщика)
 const Greeting: React.FC<GreetingPropsType> = (
-    {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
+  {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
 ) => {
-    const inputClass = s.error // need to fix with (?:)
-
-    return (
-        <div>
-            <input value={name} onChange={setNameCallback} className={inputClass}/>
-            <span>{error}</span>
-            <button onClick={addUser}>add</button>
-            <span>{totalUsers}</span>
-        </div>
-    )
+  const inputClass = (error === "Name should not be blank!") ? s.error : ""// need to fix with (?:)
+  const errorStyle = {
+    margin: 0,
+    paddingLeft: "5px",
+    color: error === "Name should not be blank!" ? "red" :"black",
+  }
+  return (
+    <div>
+      <input value={name} onChange={setNameCallback} className={inputClass}/>
+      <button onClick={addUser}>add</button>
+      <span>{totalUsers}</span>
+      <p style={errorStyle}><span>{error}</span></p>
+    </div>
+  )
 }
 
 export default Greeting
--- src/p2-homeworks/h3/GreetingContainer.tsx
+++ src/p2-homeworks/h3/GreetingContainer.tsx
@@ -1,38 +1,48 @@
-import React, {useState} from 'react'
+import React, {useState, ChangeEvent} from 'react'
 import Greeting from './Greeting'
+import {UserType} from "./HW3";
 
 type GreetingContainerPropsType = {
-    users: any // need to fix any
-    addUserCallback: any // need to fix any
+  users: Array<UserType> // need to fix any
+  addUserCallback: (name: string) => void // need to fix any
 }
 
 // более простой и понятный для новичков
 // function GreetingContainer(props: GreetingPropsType) {
-
+export type errorType = "Name should not be blank!" | "Type in your name."
 // более современный и удобный для про :)
 // уровень локальной логики
 const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUserCallback}) => { // деструктуризация пропсов
-    const [name, setName] = useState<any>('') // need to fix any
-    const [error, setError] = useState<any>('') // need to fix any
+  const [name, setName] = useState<string>('') // need to fix any
+  const [error, setError] = useState<errorType>('Type in your name.') // need to fix any
 
-    const setNameCallback = (e: any) => { // need to fix any
-        setName('') // need to fix
+  const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => { // need to fix any
+    setName(e.currentTarget.value) // need to fix
+  }
+  const addUser = () => {
+    if (name.trim().length === 0) {
+      setError("Name should not be blank!")
+      setName("")
+    } else {
+      alert(`Hello ${name.trim()} !`) // need to fix
+      setError("Type in your name.")
+      addUserCallback(name.trim())
+      setName("")
     }
-    const addUser = () => {
-        alert(`Hello  !`) // need to fix
-    }
 
-    const totalUsers = 0 // need to fix
+  }
 
-    return (
-        <Greeting
-            name={name}
-            setNameCallback={setNameCallback}
-            addUser={addUser}
-            error={error}
-            totalUsers={totalUsers}
-        />
-    )
+  const totalUsers = users.length // need to fix
+
+  return (
+    <Greeting
+      name={name}
+      setNameCallback={setNameCallback}
+      addUser={addUser}
+      error={error}
+      totalUsers={totalUsers}
+    />
+  )
 }
 
 export default GreetingContainer
