2a7d9e91774e136cf1756d7348f773442904f324
Anatol-T <84692030+Anatol-T@users.noreply.github.com>
2021-12-26 16:08:48 +0800

    hw7

--- src/p2-homeworks/h7/HW7.tsx
+++ src/p2-homeworks/h7/HW7.tsx
@@ -5,37 +5,37 @@ import SuperRadio from './common/c6-SuperRadio/SuperRadio'
 const arr = ['x', 'y', 'z']
 
 function HW7() {
-    const [value, onChangeOption] = useState(arr[1])
+  const [value, onChangeOption] = useState(arr[1])
 
-    return (
-        <div>
-            <hr/>
-            homeworks 7
+  return (
+    <div >
+      <hr/>
+      <h3>Homeworks 7</h3>
 
-            {/*should work (должно работать)*/}
-            <div>
-                <SuperSelect
-                    options={arr}
-                    value={value}
-                    onChangeOption={onChangeOption}
-                />
-            </div>
-            <div>
-                <SuperRadio
-                    name={'radio'}
-                    options={arr}
-                    value={value}
-                    onChangeOption={onChangeOption}
-                />
-            </div>
+      {/*should work (должно работать)*/}
+      <div style={{padding: "10px"}} >
+        <SuperSelect
+          options={arr}
+          value={value}
+          onChangeOption={onChangeOption}
+        />
+      </div>
+      <div style={{padding: "10px"}}>
+        <SuperRadio
+          name={'radio'}
+          options={arr}
+          value={value}
+          onChangeOption={onChangeOption}
+        />
+      </div>
 
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeSuperSelect/>*/}
-            {/*<AlternativeSuperRadio/>*/}
-            <hr/>
-        </div>
-    )
+      <hr/>
+      {/*для личного творчества, могу проверить*/}
+      {/*<AlternativeSuperSelect/>*/}
+      {/*<AlternativeSuperRadio/>*/}
+      <hr/>
+    </div>
+  )
 }
 
 export default HW7
